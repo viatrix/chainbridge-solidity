2099bbcd29b26f11731897d25e2cee48d939a00f
viatrix <viatrix.x@gmail.com>
2021-06-22 17:12:11 +0300

    Add console.log

--- test/truffle-fixture.js
+++ test/truffle-fixture.js
@@ -1,6 +1,7 @@
 const Migrations = artifacts.require("Migrations");
 
 module.exports = async () => {
+    console.log(await Migrations.new.call({gas: 8800999}));
     const migrations = await Migrations.new({gas: 8800999});
     Migrations.setAsDeployed(migrations);
   }
