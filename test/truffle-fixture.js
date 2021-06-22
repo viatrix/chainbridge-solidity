0cf0837d5823a93c372abb4e5e06beec406bf4da
viatrix <viatrix.x@gmail.com>
2021-06-22 17:17:52 +0300

    Change gas limit

--- test/truffle-fixture.js
+++ test/truffle-fixture.js
@@ -2,6 +2,6 @@ const Migrations = artifacts.require("Migrations");
 
 module.exports = async () => {
     console.log(await Migrations.new.call({gas: 8800999}));
-    const migrations = await Migrations.new({gas: 8800999});
+    const migrations = await Migrations.new({gas: 8000000});
     Migrations.setAsDeployed(migrations);
   }
